f094f17c702f1c843fd27e579b4aa6fb7dd7dbb2
ethan015 <ethanalexander13@hotmail.com>
2017-03-07 18:20:48 +0100

    Update VentanaMedico.java

--- VentanaMedico.java
+++ VentanaMedico.java
@@ -1,6 +1,7 @@
 package Vista;
 
 import java.awt.Rectangle;
+
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,12 +21,14 @@ import javax.swing.table.TableColumn;
 import Control.ControladorPrincipal;
 import Control.LoginControl;
 import Control.ManejadorDeFicheros;
+import Model.CellEditable;
 import Model.GestionDatos;
 import Model.Login;
 import javax.swing.GroupLayout.Alignment;
 import javax.print.DocFlavor.URL;
 import javax.swing.GroupLayout;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JLabel;
@@ -38,6 +41,9 @@ import java.awt.EventQueue;
 import javax.swing.Icon;
 import javax.swing.JTextArea;
 import java.awt.Frame;
+import Model.Medico;
+import javax.swing.border.BevelBorder;
+import javax.swing.plaf.FontUIResource;
 
 /**
  *
@@ -47,8 +53,7 @@ public class VentanaMedico extends javax.swing.JFrame {
 
     /**
      * Creates new form VentanaMedico
-     */
-	private ArrayList<String[]> pacientes;                     
+     */                     
     private javax.swing.JButton botonBuscador;
     private javax.swing.JLabel lblBuscador;
     private javax.swing.JScrollPane scrollTabla;
@@ -60,7 +65,6 @@ public class VentanaMedico extends javax.swing.JFrame {
 	public JTextField campoApellido;
 	private JPanel panelfoto;
 	private JLabel lblfoto;
-	private JLabel lblseparador;
 	private JTextArea txtInfoMedico;
 	public String nombre;
 	public String apellidos;
@@ -69,24 +73,24 @@ public class VentanaMedico extends javax.swing.JFrame {
 	private String resulInfo;
 	private JLabel lblSlimUem;
 	private JLabel lblRellenaNom;
-	private JLabel lblApell;
-	private JLabel lblNº;
-	private JLabel lblFecha;
+	private JLabel lblRellenaNÂº;
+	private JLabel lblRellenaApell;
+	private Medico medico;
+	private JTextField textNombre;
+	private JTextField textRellenaApellido;
+	private JTextField textrellenaFecha;
+	private JTextField textRellenaNÂº;
+	private JLabel lblNewLabel;
 
-	/*public void VentanaMedico(String nombre,String apellidos,String id,String cumpleaños){
-		this.nombre=nombre;
-		this.apellidos=apellidos;
-		this.id=id;
-		this.cumpleaños=cumpleaños;
-	}*/
-	
-	
+		
 	/**
 	 * 
 	 */
-	public VentanaMedico(ArrayList<String[]> pacientes) {
+	public VentanaMedico(Medico medico) {
+		this.medico = medico;
+		
 		setExtendedState(6);
-		this.pacientes = pacientes;
+		
 		
 		//icono CardioStrike
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\icono.png"));
@@ -94,23 +98,35 @@ public class VentanaMedico extends javax.swing.JFrame {
 		
 	
     	initComponents();
-    	setLocationRelativeTo(null);
-    	//setResizable(false);
-    	//setTitle();
     	
+    	setLocationRelativeTo(null);
+    	panelfoto.setLayout(null);
+    	
+    	//foto medico
     	lblfoto = new JLabel("");
-    	lblfoto.setBounds(0, 0, 235, 172);
+    	lblfoto.setBounds(-71, 0, 413, 425);
     	panelfoto.add(lblfoto);
     	lblfoto.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\logoMedico.png"));
     	this.repaint();
     	
+    	//foto logo
     	ImageIcon imagen = new ImageIcon("src/imagenes/logoSlimUem.jpg");
         Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(lblSlimUem.getWidth(),lblSlimUem.getHeight(),Image.SCALE_DEFAULT));
         lblSlimUem.setIcon(icono);
         this.repaint();
-    }
+        
+        //Redimensionar fotos campos
+        ImageIcon imagen2 = new ImageIcon("src/Imagenes/txt_grande.png");
+        this.repaint();
+        
+        textNombre.setText(medico.getNombre());
+        textRellenaNÂº.setText(medico.getnColegiado());
+        textRellenaApellido.setText(medico.getApellidos());
+        textrellenaFecha.setText(medico.getFechaNacimiento());
+        
+	}
     
-  
+  //icono
 	public Image getIconImage() {
     	   Image retValue = Toolkit.getDefaultToolkit().
     	         getImage(ClassLoader.getSystemResource("resources/icono.png"));
@@ -120,130 +136,120 @@ public class VentanaMedico extends javax.swing.JFrame {
     	}
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+                             
     private void initComponents() {
-    	 //this.setExtendedState(MAXIMIZED_BOTH);
-    	 //this.setUndecorated(true);
-    	  
+    	
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    
         
-       
-       
-        //tabla.setDefaultRenderer(Object.class, new Render());
-
-        //JButton botonSelect = new JButton("Seleccionar");
-        
-       // DefaultTableModel d= new DefaultTableModel();
-        
-        Object[][] contenidoModelo = new Object[pacientes.size()][3];
-        for(int i = 0; i < pacientes.size(); i++){
-        	contenidoModelo[i][0] =null;
-		//frame.setVisible(true);
-		//	frame.toFront();;//Imagenes Crear clase Render que sea implementada
-        	contenidoModelo[i][1] = pacientes.get(i)[0];
-        	contenidoModelo[i][2] = pacientes.get(i)[1];
+        Object[][] contenidoModelo = new Object[medico.getPacientes().size()][3];
+        for(int i = 0; i < medico.getPacientes().size(); i++){
+        	contenidoModelo[i][0] = medico.getPacientes().get(i).getNombre();
+        	contenidoModelo[i][1] = medico.getPacientes().get(i).getApellidos();
         }
         
         JPanel panelfondo = new JPanel();
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         layout.setHorizontalGroup(
         	layout.createParallelGroup(Alignment.LEADING)
-        		.addGroup(layout.createSequentialGroup()
-        			.addComponent(panelfondo, GroupLayout.PREFERRED_SIZE, 1295, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        		.addComponent(panelfondo, GroupLayout.DEFAULT_SIZE, 1914, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
         	layout.createParallelGroup(Alignment.LEADING)
-        		.addGroup(layout.createSequentialGroup()
-        			.addComponent(panelfondo, GroupLayout.PREFERRED_SIZE, 700, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        		.addComponent(panelfondo, GroupLayout.DEFAULT_SIZE, 1001, Short.MAX_VALUE)
         );
                 panelfondo.setLayout(null);
                 
-                JButton botonInfoMedico = new JButton("VER INFO MEDICO");
-                botonInfoMedico.addActionListener(new ActionListener() {
-                	public void actionPerformed(ActionEvent arg0) {
-                		
-                		/*
-                		File archivo = new File("C:\\Users\\ethan\\workspace\\CardioStrikee\\Data\\infoMedico.txt");
-                		try {
-            				BufferedReader leerInfoMedico = new BufferedReader(new FileReader(archivo));
-            				VentanaMedico vm;
-            				String linea = leerInfoMedico.readLine();
-            				while(linea!=null){
-            			
-	            					for(int i=0;i<4;i++){
-		            				if(linea != ";"){
-			            				txtInfo.append(linea+"\n");
-			            				linea = leerInfoMedico.readLine();
-		            				}
-	            				}
-            				}
-            			} catch (Exception ex) {
-            				
-            				ex.printStackTrace();
-            			}*/
-                		
-                	}
-                });
+                JPanel panel = new JPanel();
+                panel.setBounds(667, 85, 14, 862);
+                panelfondo.add(panel);
                 
-                lblNº = new JLabel("");
-                lblNº.setBounds(198, 495, 149, 28);
-                panelfondo.add(lblNº);
+                lblNewLabel = new JLabel("New label");
+                lblNewLabel.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\fotoBlanca.png"));
+                panel.add(lblNewLabel);
                 
-                lblApell = new JLabel("New label");
-                lblApell.setBounds(198, 438, 122, 39);
-                panelfondo.add(lblApell);
+                textNombre = new JTextField();
+                textNombre.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                textNombre.setBorder(null);
+                textNombre.setBounds(309, 417, 202, 16);
+                panelfondo.add(textNombre);
+                textNombre.setColumns(10);
+                
+                textrellenaFecha = new JTextField();
+                textrellenaFecha.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                textrellenaFecha.setBorder(null);
+                textrellenaFecha.setBounds(309, 881, 202, 16);
+                panelfondo.add(textrellenaFecha);
+                textrellenaFecha.setColumns(10);
+                
+                textRellenaNÂº = new JTextField();
+                textRellenaNÂº.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                textRellenaNÂº.setBorder(null);
+                textRellenaNÂº.setBounds(309, 725, 187, 16);
+                panelfondo.add(textRellenaNÂº);
+                textRellenaNÂº.setColumns(10);
+                
+                textRellenaApellido = new JTextField();
+                textRellenaApellido.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                textRellenaApellido.setBorder(null);
+                textRellenaApellido.setBounds(309, 567, 187, 16);
+                panelfondo.add(textRellenaApellido);
+                textRellenaApellido.setColumns(10);
+                
+                JLabel label_1 = new JLabel("");
+                label_1.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\txt_grande.png"));
+                label_1.setBounds(300, 864, 259, 50);
+                panelfondo.add(label_1);
+                
+                lblRellenaApell = new JLabel("");
+                lblRellenaApell.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\txt_grande.png"));
+                lblRellenaApell.setBounds(300, 556, 249, 42);
+                panelfondo.add(lblRellenaApell);
+                
+                lblRellenaNÂº = new JLabel("");
+                lblRellenaNÂº.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\txt_grande.png"));
+                lblRellenaNÂº.setBounds(300, 713, 259, 43);
+                panelfondo.add(lblRellenaNÂº);
                 
                 lblRellenaNom = new JLabel("");
-                lblRellenaNom.setBounds(198, 379, 106, 35);
+                lblRellenaNom.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\txt_grande.png"));
+                lblRellenaNom.setBounds(300, 388, 242, 65);
                 panelfondo.add(lblRellenaNom);
-                
-                lblFecha = new JLabel("New label");
-                lblFecha.setBounds(208, 553, 156, 37);
-                panelfondo.add(lblFecha);
                 	
                 
                 JLabel lblFechaNacimieto = new JLabel("Fecha nacimieto");
-                lblFechaNacimieto.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                lblFechaNacimieto.setFont(new Font("Tahoma", Font.PLAIN, 30));
                 lblFechaNacimieto.setForeground(Color.WHITE);
-                lblFechaNacimieto.setBounds(37, 547, 183, 42);
+                lblFechaNacimieto.setBounds(31, 864, 249, 42);
                 panelfondo.add(lblFechaNacimieto);
                 
                 JLabel lblNidentificador = new JLabel("N\u00AAIdentificador");
-                lblNidentificador.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                lblNidentificador.setFont(new Font("Tahoma", Font.PLAIN, 30));
                 lblNidentificador.setForeground(Color.WHITE);
-                lblNidentificador.setBounds(37, 481, 135, 53);
+                lblNidentificador.setBounds(31, 703, 211, 53);
                 panelfondo.add(lblNidentificador);
                 
                 JLabel lblApellidos = new JLabel("Apellidos");
-                lblApellidos.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                lblApellidos.setFont(new Font("Tahoma", Font.PLAIN, 30));
                 lblApellidos.setForeground(Color.WHITE);
-                lblApellidos.setBounds(37, 440, 85, 28);
+                lblApellidos.setBounds(31, 545, 135, 53);
                 panelfondo.add(lblApellidos);
                 
                 JLabel lblNombre = new JLabel("Nombre");
-                lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 30));
                 lblNombre.setForeground(Color.WHITE);
-                lblNombre.setBounds(37, 369, 85, 45);
+                lblNombre.setBounds(37, 385, 127, 48);
                 panelfondo.add(lblNombre);
                 
                 lblSlimUem = new JLabel("");
                 lblSlimUem.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\logoSlimUem.jpg"));
-                lblSlimUem.setBounds(632, 94, 452, 80);
+                lblSlimUem.setBounds(903, 47, 848, 152);
                 panelfondo.add(lblSlimUem);
-                botonInfoMedico.setBounds(37, 634, 221, 25);
-                panelfondo.add(botonInfoMedico);
-                
-                lblseparador = new JLabel("");
-                lblseparador.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\linea.png"));
-                lblseparador.setBounds(447, 160, 23, 331);
-                panelfondo.add(lblseparador);
         
                 lblBuscador = new javax.swing.JLabel();
                 lblBuscador.setForeground(Color.WHITE);
-                lblBuscador.setFont(new Font("Tahoma", Font.BOLD, 22));
-                lblBuscador.setBounds(575, 239, 99, 35);
+                lblBuscador.setFont(new Font("Tahoma", Font.BOLD, 40));
+                lblBuscador.setBounds(867, 260, 187, 80);
                 panelfondo.add(lblBuscador);
                 
                         lblBuscador.setText("BUSCAR");
@@ -252,38 +258,58 @@ public class VentanaMedico extends javax.swing.JFrame {
         panelfoto.setBounds(37, 60, 267, 253);
         panelfondo.add(panelfoto);
         txtBuscador = new javax.swing.JTextField();
-        txtBuscador.setBounds(687, 248, 288, 25);
+        txtBuscador.setBounds(1124, 279, 296, 42);
         panelfondo.add(txtBuscador);
         
         botonBuscador = new JButton("Buscar");
-        botonBuscador.setBounds(1006, 248, 116, 25);
+        botonBuscador.setBounds(1499, 279, 235, 42);
         panelfondo.add(botonBuscador);
+        FontUIResource font = new FontUIResource("Verdana", Font.PLAIN, 24);
+        UIManager.put("Table.font", font);
+        UIManager.put("Table.foreground", Color.RED);
         scrollTabla = new javax.swing.JScrollPane();
-        scrollTabla.setBounds(575, 287, 547, 372);
+        scrollTabla.setBorder(null);
+        scrollTabla.setFont(scrollTabla.getFont().deriveFont(scrollTabla.getFont().getSize() + 40f));
+        scrollTabla.setBounds(867, 400, 867, 547);
         panelfondo.add(scrollTabla);
         tblLista = new javax.swing.JTable();
         
         tblLista.setModel(new DefaultTableModel(
+        		
             contenidoModelo,
+            
             new String [] {
-                "Imagen", "Nombre", "Apellidos"
+              "Nombre", "Apellidos"
             }
+            
         ));
-       
-       /* TableColumn agregarColumn;
-        agregarColumn = tblLista.getColumnModel().getColumn(5);
-        agregarColumn.setCellEditor(new myeditor(tblLista));
-        agregarColumn.setCellRenderer(new Render(true));
-        */
+        tblLista.setCursor(new java.awt.Cursor(java.awt.Cursor.MOVE_CURSOR));
+        tblLista.setIntercellSpacing(new java.awt.Dimension(5, 5));
+        
+        tblLista.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+            if(evt.getClickCount()==2){
+            	VentanaPaciente frame = new VentanaPaciente();
+				frame.setVisible(true);
+				frame.toFront();
+			}
+				
+           }
+        });
         scrollTabla.setViewportView(tblLista);
+        tblLista.setRowHeight(70);
+     
+       // tblLista myTable = new tblLista(new CellEditable());
+                  	
+
         
         JLabel label = new JLabel("");
         label.setIcon(new ImageIcon("C:\\Users\\ethan\\workspace\\CardioStrikee\\src\\Imagenes\\fondoMedico.jpg"));
-        label.setBounds(0, 0, 1295, 700);
+        label.setBounds(0, 13, 1915, 1001);
         panelfondo.add(label);
         botonBuscador.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
-        		int pos = GestionDatos.buscarPaciente(txtBuscador.getText(), pacientes);
+        		int pos = GestionDatos.buscarPaciente(txtBuscador.getText(), medico.getPacientes());
         		if(pos != GestionDatos.NOENCONTRADO){
         			tblLista.getSelectionModel().setSelectionInterval (pos, pos);
         			Rectangle rect = tblLista.getCellRect(pos, 0, true);
@@ -302,45 +328,8 @@ public class VentanaMedico extends javax.swing.JFrame {
         pack();
     }// </editor-fold>                        
 
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	//NO ME SALE HACERLO MODELO VISTA CONTROLADOR
-	/*private void botonInfoMedicoActionPerformed(java.awt.event.ActionEvent evt){
-		ControladorPrincipal cp=new ControladorPrincipal(this);//llamo al propio objeto de esta VentanaPrincipal principal
-		
-        //Leer info
- 		cp.mf = new ManejadorDeFicheros();
- 		resulInfo = cp.mf.infoMedico("C:\\Users\\ethan\\workspace\\CardioStrikee\\Data\\infoMedico.txt", ";");
- 	
-		
-		if(resulInfo == resultado){
-    		
-	    	//clase capaz de encolar eventos de forma asíncrona.
-	    	EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						File archivo = new File("C:\\Users\\ethan\\workspace\\CardioStrikee\\Data\\infoMedico.txt");
-						BufferedReader leerInfoMedico = new BufferedReader(new FileReader(archivo));
-        				String linea = leerInfoMedico.readLine();
-        				while(linea!=null){
-        				//	VentanaMedico vm = new VentanaMedico();
-        				
-        				txtInfo.append(linea+"\n");
-        				linea = leerInfoMedico.readLine();
-        				}
-					
-						
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});
-	    	}
-	}*/
+	
+	
     
     public void addController (LoginControl lc){
 	   	controlador = lc;
