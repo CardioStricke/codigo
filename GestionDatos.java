3531ee44d8a1fcb873f853ac16817a3ccc24e7f0
ethan015 <ethanalexander13@hotmail.com>
2017-03-22 22:19:34 +0100

    Update GestionDatos.java

--- GestionDatos.java
+++ GestionDatos.java
@@ -4,18 +4,33 @@ import java.util.ArrayList;
 
 public class GestionDatos {
 	public static final int NOENCONTRADO = -1;
-	public static int buscarPaciente(String nombreCompleto, ArrayList<String[]> pacientes){
+	public static int buscarPaciente(String nombreCompleto, ArrayList<Paciente> pacientes){
 		boolean encontrado = false;
 		int i= 0;
 		int pos = NOENCONTRADO;
 		while(!encontrado && i < pacientes.size()){
-			if(nombreCompleto.equals(pacientes.get(i)[0])||nombreCompleto.equals(pacientes.get(i)[0] +" "+pacientes.get(i)[1])){
+			if(nombreCompleto.equals(pacientes.get(i).getNombre())||nombreCompleto.equals(pacientes.get(i).getNombre() +" "+pacientes.get(i).getApellidos())){
 				encontrado = true;
 				pos = i;
 			}
 			i++;
 		}
 		return pos;
+	}
+	public static ArrayList<Tiempo> obtenerTiempos(ArrayList<Sesion> sesiones){
+		ArrayList<Tiempo> tiempos = new ArrayList<Tiempo>();
+		for(Sesion s: sesiones){
+			tiempos.add(s.getTiempo());
+		}
+		return tiempos;
+	}
+	
+	public static ArrayList<Float> obtenerVelocidades(ArrayList<Sesion> sesiones){
+		ArrayList<Float> velocidades = new ArrayList<Float>();
+		for(Sesion s: sesiones){
+			velocidades.add(new Float(s.getVelocidad()));
+		}
+		return velocidades;
 	}
 
 }
