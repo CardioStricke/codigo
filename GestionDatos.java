ea8974c7d94d137cb877b698c36845cd58b0a791
guillerodriguez3110 <guillerodriguez3110@gmail.com>
2017-03-23 11:58:17 +0100

    Update GestionDatos.java

--- GestionDatos.java
+++ GestionDatos.java
@@ -2,7 +2,24 @@ package Model;
 
 import java.util.ArrayList;
 
+/**
+ * Esta clase gestiona los datos de los ficheros
+ *@author Ethan Recalde
+ *@version 2.0
+ */
+
 public class GestionDatos {
+   
+	/**
+     * Este metodo busca pacientes en el array de pacientes, se utiliza para el buscador el login medico
+     * @author Ethan Recalde
+     * @param NOENCONTRADO inicializa la posicion como no encontrado (por eso es -1)
+     * @param nombreCompleto nombre completo del paciente
+     * @param pacientes array en el que se encuentran los pacientes
+     * @param i contador utilizado en el bucle
+     * @return pos devuelve la posicion en la que se encuentra el paciente 
+     * @version 2.0
+     */
 	public static final int NOENCONTRADO = -1;
 	public static int buscarPaciente(String nombreCompleto, ArrayList<Paciente> pacientes){
 		boolean encontrado = false;
@@ -17,6 +34,13 @@ public class GestionDatos {
 		}
 		return pos;
 	}
+	
+	/**
+     * Este metodo obtiene los tiempos de la sesion en un array de tiempos
+     * @author Ethan Recalde
+     * @return tiempos devuelve el array de tiempos
+     * @version 1.0
+     */
 	public static ArrayList<Tiempo> obtenerTiempos(ArrayList<Sesion> sesiones){
 		ArrayList<Tiempo> tiempos = new ArrayList<Tiempo>();
 		for(Sesion s: sesiones){
@@ -25,6 +49,12 @@ public class GestionDatos {
 		return tiempos;
 	}
 	
+	/**
+     * Este metodo obtiene las velocidades de la sesion en un array de velocidades, se utiliza en FreeChartVelocidad para pintar la grafica
+     * @author Ethan Recalde
+     * @return velocidades devuelve el array de velocidades
+     * @version 1.0
+     */
 	public static ArrayList<Float> obtenerVelocidades(ArrayList<Sesion> sesiones){
 		ArrayList<Float> velocidades = new ArrayList<Float>();
 		for(Sesion s: sesiones){
@@ -34,6 +64,12 @@ public class GestionDatos {
 	}
 	
 
+	/**
+     * Este metodo obtiene las alturas de la sesion en un array de distancias, se utiliza en FreeChartAltura para pintar la grafica
+     * @author Ethan Recalde
+     * @return altura devuelve el array de alturas
+     * @version 1.0
+     */
 	public static ArrayList<Float> obtenerDistancia(ArrayList<Sesion> sesiones){
 		ArrayList<Float> altura = new ArrayList<Float>();
 		for(Sesion s: sesiones){
@@ -42,6 +78,12 @@ public class GestionDatos {
 		return altura;
 	}
 	
+	/**
+     * Este metodo obtiene las pulsaciones en la sesion en un array de pulsaciones, se utiliza en FreeChartPulsaciones para pintar la grafica
+     * @author Ethan Recalde
+     * @return pulsaciones devuelve el array de pulsaciones
+     * @version 1.0
+     */
 	public static ArrayList<Integer> obtenerPulsaciones(ArrayList<Sesion> sesiones){
 		ArrayList<Integer> pulsaciones = new ArrayList<Integer>();
 		for(Sesion s: sesiones){
@@ -50,6 +92,12 @@ public class GestionDatos {
 		return pulsaciones;
 	}
 	
+	/**
+     * Este metodo obtiene el nivel de oxigeno en sangre en la sesion en un array de enteros (o2 sangre), se utiliza en FreeChartO2Sangre para pintar la grafica
+     * @author Ethan Recalde
+     * @return sangre devuelve el array de enteros(o2 sangre)
+     * @version 1.0
+     */
 	public static ArrayList<Integer> obtenerOxigenoSangre(ArrayList<Sesion> sesiones){
 		ArrayList<Integer> sangre = new ArrayList<Integer>();
 		for(Sesion s: sesiones){
