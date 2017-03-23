d5e049ae5058ffd7052631b287a65eb155d509ca
ethan015 <ethanalexander13@hotmail.com>
2017-03-23 11:32:08 +0100

    Update ControladorPrincipal.java

--- ControladorPrincipal.java
+++ ControladorPrincipal.java
@@ -18,24 +18,39 @@ import java.awt.List;
 
 public class ControladorPrincipal implements ActionListener
 {
-	
+	/**
+	 * Atributos del controlador
+	 * @param ventanaControlada
+	 * @param mf
+	 * @param userlist
+	 * @param list1
+	 * */
 	public JFrame ventanaControlada;
 	public ManejadorDeFicheros mf;
+	
+	//Es un componente Swing que muestra una lista desplegable de opciones y permite al usuario seleccionar un elemento de la lista.
 	public JComboBox <Login> usersList;
 	private List list1;
 	
+	/**
+	 * Asiganmos el control a la ventana
+	 * */
 	public ControladorPrincipal(JFrame win){
 		ventanaControlada=win;
 			}
 	
-	
+	/**
+	 * Definimos el evento 
+	 * */
 	public void actionPerformed(ActionEvent e) {
 		
     	String cmd = (String)(e.getActionCommand());
         abrirVentana(cmd);
 		
 	} 
-    
+    /**
+     * asignamos el control al medico
+     * */
     public void abrirVentana(String cmd)
 	{
     	// Creo la ventana para representarlo
@@ -47,12 +62,15 @@ public class ControladorPrincipal implements ActionListener
 		// Le Asociamos el controlador a la ventana
 		med.addController(lc);
 			
-		// La añado a la ventana principal
+		// La aÃ±ado a la ventana principal
 		ventanaControlada.getContentPane().add(med);
 		med.toFront();
 				
 	}	
 
+    /**
+     * Metodo que permite cerrar la ventana
+     * */
 	public void cerrarVentana()
 	{
 		ventanaControlada.dispose();
